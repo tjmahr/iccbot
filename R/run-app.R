e49dc4205f206c8cc19db3d1dfbfa670755a437d
TJ Mahr <mahr@wisc.edu>
2019-09-20 10:29:14 -0500

    set default app

--- R/run-app.R
+++ R/run-app.R
@@ -5,6 +5,7 @@ run_app <- function(...) {
   app_path <- system.file("app/app.Rmd", package = "iccbot")
   rmarkdown::run(
     app_path,
+    default_file = "app.Rmd",
     shiny_args = list(launch.browser = TRUE)
   )
 }
